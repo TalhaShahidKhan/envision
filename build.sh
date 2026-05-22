cf623e3d02f7d7d8a8f6ac251e49b421dca6fb6c
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2026-05-22 20:36:19 +0600

    Fix STATIC_URL and build.sh virtual env

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "Installing dependencies..."
 python -m pip install --upgrade pip
 pip install -r requirements.txt
